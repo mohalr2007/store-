7cbbae9b7f62b566bd0407a3f4ee91461c25774d
mohalr2007 <mohalr2007@users.noreply.github.com>
2026-07-08 18:32:33 +0100

    fix(store): wrap MetaPixel with Suspense to resolve Next.js build error

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
 
@@ -14,7 +14,7 @@ declare global {
 
 const FB_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
-export const MetaPixel = () => {
+const PixelContent = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const [isLoaded, setIsLoaded] = useState(false);
@@ -29,25 +29,31 @@ export const MetaPixel = () => {
   if (!FB_PIXEL_ID) return null;
 
   return (
-    <>
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${FB_PIXEL_ID}');
-          `,
-        }}
-        onLoad={() => setIsLoaded(true)}
-      />
-    </>
+    <Script
+      id="fb-pixel"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${FB_PIXEL_ID}');
+        `,
+      }}
+      onLoad={() => setIsLoaded(true)}
+    />
+  );
+};
+
+export const MetaPixel = () => {
+  return (
+    <Suspense fallback={null}>
+      <PixelContent />
+    </Suspense>
   );
 };
